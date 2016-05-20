f269d5270b929153d4e244acfbb5130bf66a0718
Davide Orlando <davide.orl94@gmail.com>
2016-05-20 12:26:01 +0200

    [Facade-THWEb] Aggiunta!

---
+++ src/it/uniroma3/model/THWeb.java
@@ -0,0 +1,23 @@
+package it.uniroma3.model;
+
+import java.util.List;
+
+public class THWeb {
+
+	public Medico findMedico(String codiceMedico){
+		return null;
+		}
+	
+	public Paziente findPaziente(String codiceFiscale){
+		return null;
+		}
+	
+	public List<Medico> getMedici(){
+		return null;
+	}
+	public List<TipologiaEsame> getTipologieEsame(){
+		return null;
+	}
+	
+	public void addPrenotazione(Paziente paziente, Medico medico, TipologiaEsame tipologiaEsame){}
+}
